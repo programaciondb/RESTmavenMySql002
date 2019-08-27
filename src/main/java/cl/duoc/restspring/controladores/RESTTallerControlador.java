abc0e3b69324a6c67ba1f7c9c861b7635ac3d6f4
David Bousquet <>
2019-08-27 16:45:16 -0300

    Se termina CRUD Taller y listar Matricula

--- src/main/java/cl/duoc/restspring/controladores/RESTTallerControlador.java
+++ src/main/java/cl/duoc/restspring/controladores/RESTTallerControlador.java
@@ -8,9 +8,14 @@ package cl.duoc.restspring.controladores;
 import cl.duoc.restspring.modelos.TallerEntidad;
 import cl.duoc.restspring.modelos.TallerRepositorio;
 import java.util.List;
+import java.util.Map;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -22,7 +27,7 @@ public class RESTTallerControlador {
     @GetMapping("/")
     public String main(){
         return "Talleres";
-    }
+    }                                       
     
     @GetMapping("/talleres")
     public List<TallerEntidad> listar(){
@@ -31,5 +36,31 @@ public class RESTTallerControlador {
     @GetMapping("/talleres/{id}")
     public TallerEntidad listar(@PathVariable Integer id){
         return tr.findById(id).orElse(new TallerEntidad());
+    }
+    
+    @PostMapping("/talleres")
+    public TallerEntidad nuevo(@RequestBody Map<String, String> dato){
+        TallerEntidad t = new TallerEntidad();
+        t.setNombre(dato.get("nombre"));
+        t.setDetalle(dato.get("detalle"));
+        t.setNumhoras(Integer.parseInt(dato.get("numhoras")));
+        t.setCupos(Integer.parseInt(dato.get("cupos")));
+        return tr.save(t);
+    }
+    @PutMapping("/talleres/{id}")
+    public TallerEntidad modificar(@RequestBody Map<String, String> dato, @PathVariable Integer id){
+        TallerEntidad t = new TallerEntidad();
+        t.setNumtaller(id);
+        t.setNombre(dato.get("nombre"));
+        t.setDetalle(dato.get("detalle"));
+        t.setNumhoras(Integer.parseInt(dato.get("numhoras")));
+        t.setCupos(Integer.parseInt(dato.get("cupos")));
+        return tr.save(t);
+    }
+            
+    @DeleteMapping("/talleres/{id}")
+    public boolean borrar(@PathVariable Integer id){
+        tr.deleteById(id);
+        return !tr.existsById(id);
     }
 }
