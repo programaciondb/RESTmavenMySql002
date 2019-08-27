3cbed4b8782b4f5a9be42b6b8658b3a49001c369
David Bousquet <>
2019-08-27 16:58:42 -0300

    Se termina CRUD Taller y listar Matricula

--- src/main/java/cl/duoc/restspring/controladores/RESTTallerControlador.java
+++ src/main/java/cl/duoc/restspring/controladores/RESTTallerControlador.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package cl.duoc.restspring.controladores;
 
 import cl.duoc.restspring.modelos.TallerEntidad;
@@ -10,15 +5,18 @@ import cl.duoc.restspring.modelos.TallerRepositorio;
 import java.util.List;
 import java.util.Map;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@CrossOrigin(origins="*", methods={RequestMethod.GET, RequestMethod.POST, RequestMethod.PUT, RequestMethod.DELETE})
 public class RESTTallerControlador {
     
     @Autowired
